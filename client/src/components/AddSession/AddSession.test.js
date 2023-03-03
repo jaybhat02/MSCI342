18ac6f18f7389daabf8db5b54c8a379d37b86a74
amaalbhanji <a7bhanji@uwaterloo.ca>
2023-03-03 16:12:39 -0500

    Working unit test for Add Session button

--- client/src/components/AddSession/AddSession.test.js
+++ client/src/components/AddSession/AddSession.test.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { render, getByRole, getByTestId, fireEvent, toBeInTheDocument, toBeEqual } from "@testing-library/react";
-import { toBeInTheDocument} from '@testing-library/jest-dom/extent-expect';
+import { render, getByRole, getByTestId, fireEvent, toBeEqual } from "@testing-library/react";
+import { toBeInTheDocument } from '@testing-library/jest-dom'
 import AddSession from './AddSession';
 
 
 test("AddSession", () => {
     const { getByTestId } = render(<AddSession />);
-    const handleClick = jest.fn();
     const buttonElement = getByTestId('addSessionButton');
-    // const buttonElement = screen.getByRole('button');
     fireEvent.click(buttonElement);
     expect(getByTestId('addSessionModal')).toBeInTheDocument();
-   // expect(toBeInTheDocument(getByTestId('addSessionModal'))).toBeEqual(true);
+
 
 }); 
