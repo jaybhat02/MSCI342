44c004b9c56f4a064881822f0052755422effa77
“jaybhat02” <jbhatasa@uwaterloo.ca>
2023-03-21 17:14:25 -0400

    changes

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -55,7 +55,7 @@ const NavigationBar = (props) => {
                     <Link
                         color="inherit"
                         style={{ cursor: "pointer", margin: '20px' }}
-                        onClick={() => history.push('/Upcoming')}
+                        
                     >
                         <Typography variant="body1" component="div" sx={{ flexGrow: 1 }}>
                             Upcoming
@@ -64,7 +64,7 @@ const NavigationBar = (props) => {
                     <Link
                         color="inherit"
                         style={{ cursor: "pointer", margin: '15px' }}
-                        onClick={() => history.push('/Previous')}
+                        
                     >
                         <Typography variant="body1" component="div" sx={{ flexGrow: 1 }}>
                             Previous
